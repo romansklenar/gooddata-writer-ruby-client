4e68e8416fae0c552086a15ff8d847396a60c0be
Roman Sklenář <mail@romansklenar.cz>
2015-06-18 14:30:56 +0200

    Generated gem skeleton
    bundle gem keboola-gooddata_writer

---
+++ lib/keboola/gooddata_writer.rb
@@ -0,0 +1,7 @@
+require "keboola/gooddata_writer/version"
+
+module Keboola
+  module GooddataWriter
+    # Your code goes here...
+  end
+end
---
+++ lib/keboola/gooddata_writer/version.rb
@@ -0,0 +1,5 @@
+module Keboola
+  module GooddataWriter
+    VERSION = "0.1.0"
+  end
+end
